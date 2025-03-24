d00fd183af7103227ef5bac24783edb92e3394c7
Abudu Samadu <abudu.samadu@amalitech.com>
2025-03-24 08:06:38 +0000

    first commit

---
+++ src/main/java/com/mascot/s3gallerywithdb/S3GalleryWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.mascot.s3gallerywithdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3GalleryWithDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3GalleryWithDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mascot/s3gallerywithdb/S3GalleryWithDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mascot.s3gallerywithdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3GalleryWithDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
